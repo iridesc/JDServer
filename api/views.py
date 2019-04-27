2929978ff3307a7f772abf201eccddcadfe384aa
Irid <lielang6@live.com>
2019-04-28 00:52:21 +0800

    ok

--- api/views.py
+++ api/views.py
@@ -282,7 +282,7 @@ def RemoveExistingActivityId(data):
 
     return_data={
         'Status':True,
-        'ActivityIdList':new_activity_id_list[0:10]
+        'ActivityIdList':new_activity_id_list
     }
     print(len(activity_id_list),' -> ',len(new_activity_id_list),end=' ')
     print('Done .')
