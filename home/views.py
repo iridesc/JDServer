55da1824bfca9c33fcbea29d627406ae557344c8
Irid <lielang6@live.com>
2019-04-27 19:24:24 +0800

    优化

--- home/views.py
+++ home/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 def home(request):
     with open('./nohup.out') as f:
         lines=f.readlines()
+    if len(lines)>500:
+        lines[-1,-500]
     html=''
     for line in lines:
         html+='<li>{}</li>'.format(line)
